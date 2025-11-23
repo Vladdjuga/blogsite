4d407d3b2d0bb7ac15a344b61506e5892096a06e
Vladdjuga <vladdjuga@gmail.com>
2025-11-23 23:15:35 +0200

    feat: add created_at column to users table if it does not exist

--- src/main/resources/db/migration/V4__add_created_at_to_user.sql
+++ src/main/resources/db/migration/V4__add_created_at_to_user.sql
@@ -2,10 +2,9 @@
 DO $$
 BEGIN
     IF NOT EXISTS (
-        SELECT 1 FROM information_schema.columns
+        SELECT 1 FROM information_schema.columns 
         WHERE table_name = 'users' AND column_name = 'created_at'
     ) THEN
         ALTER TABLE users ADD COLUMN created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL;
     END IF;
 END $$;
-
